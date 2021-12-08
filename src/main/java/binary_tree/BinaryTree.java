5cf4d0d1be31dbc4644c78ae9a5910568fc6189b
ultamatewizard <james.lance.smith@hotmail.co.uk>
2021-12-08 12:06:48 +0000

    Added comments to BinaryTree

--- src/main/java/binary_tree/BinaryTree.java
+++ src/main/java/binary_tree/BinaryTree.java
@@ -8,6 +8,7 @@ public class BinaryTree implements BinaryTreeI{
     public BinaryTree(final int element){
         rootNode = new Node(element);
     }
+
     @Override
     public int getRootElement() {
         return rootNode.getValue();
@@ -20,10 +21,12 @@ public class BinaryTree implements BinaryTreeI{
     }
 
     private int getNumberOfElements(Node node, int total){
+        //Checks for left node, increments total, then traverses to it and recurs
         if (!node.isLeftChildEmpty()){
             total++;
             getNumberOfElements(node.getLeftChild(),total);
         }
+        //Checks for right node, increments total, then traverses to it and recurs
         if (!node.isRightChildEmpty()){
             total++;
             getNumberOfElements(node.getRightChild(),total);
@@ -38,8 +41,9 @@ public class BinaryTree implements BinaryTreeI{
 
     @Override
     public void addElements(int[] elements) {
+        //Iterates over the array of ints and adds them individually
         for (int element: elements) {
-            addNodeToTree(rootNode, element);
+            addElement(element);
         }
     }
 
@@ -51,23 +55,29 @@ public class BinaryTree implements BinaryTreeI{
 
     @Override
     public int[] getSortedTreeAsc() {
+        //Initialises an array with length equal to the number of elements in the binary tree
         int[] arr = new int[getNumberOfElements()];
+        //Resets the index to store the elements
         resetIndex();
         return getSortedTreeAsc(rootNode,arr);
     }
 
     private int[] getSortedTreeAsc(Node node, int[] arr){
+        //Checks for left node, then traverses to it and recurs
         if (!node.isLeftChildEmpty()){
             getSortedTreeAsc(node.getLeftChild(),arr);
         }
+        //Inserts the value of the node to the array at the current index
         arr[getIndex()] = node.getValue();
         incIndex();
+        //Checks for right node, then traverses to it and recurs
         if (!node.isRightChildEmpty()){
             getSortedTreeAsc(node.getRightChild(),arr);
         }
         return arr;
     }
 
+    //Index for array for sorting the binary tree
     private void resetIndex(){
         index = 0;
     }
@@ -82,17 +92,22 @@ public class BinaryTree implements BinaryTreeI{
 
     @Override
     public int[] getSortedTreeDesc() {
+        //Initialises an array with length equal to the number of elements in the binary tree
         int[] arr = new int[getNumberOfElements()];
+        //Resets the index to store the elements
         resetIndex();
         return getSortedTreeDesc(rootNode,arr);
     }
 
     private int[] getSortedTreeDesc(Node node, int[] arr){
+        //Checks for right node, then traverses to it and recurs
         if (!node.isRightChildEmpty()){
             getSortedTreeDesc(node.getRightChild(),arr);
         }
+        //Inserts the value of the node to the array at the current index
         arr[getIndex()] = node.getValue();
         incIndex();
+        //Checks for left node, then traverses to it and recurs
         if (!node.isLeftChildEmpty()){
             getSortedTreeDesc(node.getLeftChild(),arr);
         }
@@ -102,26 +117,36 @@ public class BinaryTree implements BinaryTreeI{
     private Node findNode(final int element){
         Node node = rootNode;
         while (node != null){
+            //Checks if the element we are looking for is the value of the current node, then it returns it
             if (element == node.getValue()){
                 return node;
             }
+            //Checks if the element is less than the current node's value, then it traverses to the left node
+            // Otherwise, it traverses to the right node
             if (element < node.getValue()){
                 node = node.getLeftChild();
             } else {
                 node = node.getRightChild();
             }
         }
+        //If the element is not in the binary tree, it returns null
         return null;
     }
 
     private void addNodeToTree(Node node, final int element){
+        //Checks if the element is less than or equal to the current node's value, then examines the left node
+        //Otherwise, examines the right node.
         if (element <= node.getValue()){
+            //Checks if the left child node is empty, then adds a new node to the left child of the current node
+            //Otherwise, traverses to the left node and recurs
             if (node.isLeftChildEmpty()){
                 node.setLeftChild(new Node(element));
             } else {
                 addNodeToTree(node.getLeftChild(),element);
             }
         } else if (element > node.getValue()){
+            //Checks if the right child node is empty, then adds a new node to the right child of the current node
+            //Otherwise, traverses to the right node and recurs
             if (node.isRightChildEmpty()){
                 node.setRightChild(new Node(element));
             } else {
